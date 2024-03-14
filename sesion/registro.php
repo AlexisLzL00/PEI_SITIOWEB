298bd45541c65c12a3af038d4aab7672ecfda069
AlexisLzL00 <123704171+AlexisLzL00@users.noreply.github.com>
2024-03-14 06:06:14 -0600

    Update registro.php

--- sesion/registro.php
+++ sesion/registro.php
@@ -10,42 +10,50 @@
 </head>
 <body>
 
-    <div class="login">
-        <div class="form">
-            <div class="logo-container">
-                <img src="../IMG/a.png" alt="Logo" class="logo">
-                <span class="logo-text">Progrsolab</span>
+<div class="login">
+    <div class="form">
+        <div class="logo-container">
+            <img src="../IMG/a.png" alt="Logo" class="logo">
+            <span class="logo-text">Progrsolab</span>
+        </div>
+        <h2>Registro de Usuario</h2>
+        <form action="solicitud_registro.php" method="POST">
+            <!-- Agregado un formulario con campos para el registro de usuario -->
+            <div class="input-container">
+                <i class="fas fa-user"></i>
+                <input type="text" name="username" placeholder="Username" required>
             </div>
-            <h2>Registro de Usuario</h2>
-            <form action="solicitud_registro.php" method="POST">
-                <!-- Agregado un formulario con campos para el registro de usuario -->
-                <div class="input-container">
-                    <i class="fas fa-user"></i>
-                    <input type="text" name="username" placeholder="Username" required>
-                </div>
-                <div class="input-container">
-                    <i class="fas fa-user"></i>
-                    <input type="text" name="nombre" placeholder="Nombre" required>
-                </div>
-                <div class="input-container">
-                    <i class="fas fa-envelope"></i>
-                    <input type="email" name="correo" placeholder="Correo Electrónico" required>
-                </div>
-<!-- Agregado campo de fecha de nacimiento -->
-                <div class="input-container">
-                    <i class="fas fa-calendar"></i>
-                    <input type="date" name="fecha_nacimiento" required>
-                </div>
-                <div class="input-container">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" name="password" id="password" placeholder="Password" required>
-                    <i class="fas fa-eye" id="togglePassword"></i>
-                </div>
-                <input type="submit" value="Registrar" class="submit">
-            </form>
-            <p class="terms">Al registrarte, aceptas nuestros <a href="#">Términos y Condiciones</a></p>
+            <div class="input-container">
+                <i class="fas fa-user"></i>
+                <input type="text" name="nombre" placeholder="Nombre" required>
+            </div>
+            <div class="input-container">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="correo" placeholder="Correo Electrónico" required>
+            </div>
+            <!-- Agregado campo de fecha de nacimiento -->
+            <div class="input-container">
+                <i class="fas fa-calendar"></i>
+                <input type="date" name="fecha_nacimiento" required>
+            </div>
+            <div class="input-container">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <i class="fas fa-eye" id="togglePassword"></i>
+            </div>
+            <input type="submit" value="Registrar" class="submit">
+        </form>
+        <p class="terms">¿Ya tienes un usuario? <a href="login.php">Inicia sesión</a></p>
+
+        <div class="form-check">
+            <input class="form-check-input acepto" type="checkbox" value="" id="checkboxTerminos" required>
+            <label class="form-check-label terms" for="checkboxTerminos">
+                Al registrarte, aceptas nuestros <a href="terminos.html">Términos y Condiciones</a>
+            </label>
         </div>
     </div>
+</div>
+
 
     <script>
         const togglePassword = document.getElementById('togglePassword');
